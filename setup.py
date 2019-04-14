96d643f40221b7d77304901721c4a0022b94b0aa
yuqing.ji <yuqing.ji@outlook.com>
2019-04-15 07:51:09 +0800

    init pip

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+with open('requirements.txt', 'r') as requirements_file:
+    requirements = requirements_file.read()
+
+
+setuptools.setup(
+    name='satsie',
+    version='0.1.1',
+    author='vjyq',
+    author_email='yuqing.ji@outlook.com',
+    description='a tool to check for your subscription updates.',
+    url='https://github.com/vjyq/satsie.git',
+    install_requires=requirements,
+    packages=setuptools.find_packages(include=[
+        'satsie',
+        'satsie.settings',
+        'satsie.subscription',
+        'satsie.utils',
+    ]),   
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    test_suite='./satsie/tests',
+    # entry_points={
+    #     'console_scripts': [
+    #         'satsie=satsie.utils:cli',
+    #     ],
+    # },
+)
