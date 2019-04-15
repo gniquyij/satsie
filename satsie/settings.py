eec4fce5c0fa7e51500c5f16334276004b587449
yuqing.ji <yuqing.ji@outlook.com>
2019-04-15 22:16:02 +0800

    update pip

--- satsie/settings.py
+++ satsie/settings.py
@@ -5,7 +5,8 @@ import logging
 
 
 HOME = os.path.dirname(os.path.realpath(__file__))
-SUBSCRIPTIONS_FILE = os.path.join(HOME, 'subscriptions.json')
+USER_PATH = os.path.dirname(os.path.expanduser('~user'))
+SUBSCRIPTIONS_FILE = os.path.join(USER_PATH, 'subscriptions.json')
 SUBSCRIPTIONS_FILE_FOR_TEST = os.path.join(HOME, 'tests/subscriptions.json')
 LOGS_FILE = os.path.join(HOME, 'info.log')
 
